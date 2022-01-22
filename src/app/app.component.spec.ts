5461ecbe2fb411350599d7302caf8aa936db3183
tombazan <tomasbazan95@gmail.com>
2022-01-22 18:27:49 -0300

    agregando mock de tareas y la interfaz

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -19,7 +19,7 @@ describe('AppComponent', () => {
   it(`should have as title 'task-list'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('task-list');
+
   });
 
   it('should render title', () => {
---
+++ src/app/mock-tasks.ts
@@ -0,0 +1,27 @@
+import {Task} from "./Task"
+export const TASKS: Task[] = [
+  {
+    id: 1,
+    text: "Terminar primer modulo de Angular",
+    day: 'Enero 22 a las 18:30 hs',
+    reminder: true
+  },
+  {
+    id: 2,
+    text: "Entrenar un rato",
+    day: 'Enero 22 a las 19 hs',
+    reminder: true
+  },
+  {
+    id: 3,
+    text: "Dale de comer a Leila",
+    day: 'Enero 22 a las 20:40 hs',
+    reminder: true
+  },
+  {
+    id: 4,
+    text: "Descansar",
+    day: 'Enero 22 a las 21 hs',
+    reminder: true
+  }
+]
